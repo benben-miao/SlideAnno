f4aa25f25aa624b3894e183839e44057f2475f10
benben-miao <benben.miao@outlook.com>
2025-12-12 08:52:41 +0800

    Rename project to GAnnoViz

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,4 @@
 #' @keywords internal
 .onAttach <- function(libname, pkgname) {
-	packageStartupMessage("SlideAnno 0.1.0 - Genomic sliding window annotation.")
+	packageStartupMessage("GAnnoViz 0.1.0 - Genomic annotation and visualization.")
 }
